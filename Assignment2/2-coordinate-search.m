5d211ccb188d14c6a3e7377c1e9b345c100319fd
Piyumi Weebadu Arachchige <53691448+PiyumiUoR@users.noreply.github.com>
2024-06-21 15:47:02 +0300

    Update 2-coordinate-search.m

--- Assignment2/2-coordinate-search.m
+++ Assignment2/2-coordinate-search.m
@@ -37,3 +37,22 @@ function [g_min, w_min, g_history, w_history] = coordinateSearch (g, w0, max_ite
     end
   end
 end
+
+% The function to be minimized 
+f = @(w) w(:,1).^2+ w(:,2).^2 + 2;
+
+% Starting point for the descent 
+x0 = [3 4];
+
+% Upper limit of iterations
+MAX_ITER = 10;
+
+% Call the coordinateSearch function 
+[g_min, w_min, g_history, w_history] = coordinateSearch(f, x0, MAX_ITER)
+
+% Plot the result (not mandatory, but beneficial)
+figure
+plot(g_history) 
+title('Cost history')
+xlabel('Iteration number')
+ylabel('Cost (g(w))')
